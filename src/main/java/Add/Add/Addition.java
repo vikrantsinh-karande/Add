cb857c29e97b2af19d4e3b999f008f6f816458aa
vikrantsinh-karande <viksskarande@gmail.com>
2019-05-04 22:31:14 -0500

    only Add Basic

---
+++ src/main/java/Add/Add/Addition.java
@@ -0,0 +1,18 @@
+package Add.Add;
+
+public class Addition 
+{
+	public static void main(String[] args)
+	{
+		double x,y;
+		x = 5.6;
+		y = 4.4;
+		
+		double ans;
+		
+		ans = x+y;
+		
+		System.out.println("Answer of " + x + " And "+y+" is = " + ans);
+	}
+
+}
